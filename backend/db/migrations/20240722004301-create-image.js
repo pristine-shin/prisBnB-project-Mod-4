a561749a934e84fb593df802d925436b48e72e33
Pristine Shin <prisvsyo@gmail.com>
2024-07-24 14:50:43 -0700

    updated options object for migrations for correct schema

--- backend/db/migrations/20240722004301-create-image.js
+++ backend/db/migrations/20240722004301-create-image.js
@@ -35,7 +35,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     options.tableName = "Images";
