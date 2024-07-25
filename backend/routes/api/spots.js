26a45ad77b0cf4da54684081a5c3b5b91cbc2e5e
Pristine Shin <prisvsyo@gmail.com>
2024-07-24 18:41:37 -0700

    completed get spot from id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Spot } = require('../../db/models');
+const { User, Image, Spot, Review } = require('../../db/models');
 const router = express.Router();
 router.use(express.json());
 
@@ -11,6 +11,81 @@ router.get('/', async (req, res, next) => {
     const allSpots = await Spot.findAll();
 
     res.json({"Spots": allSpots });
+})
+
+//get all spots owned/created by the current user
+router.get('/current', async (req, res, next) => {
+    const userSpots = await Spot.findAll({
+        where: {
+            //not sure about this one
+        }
+    });
+
+    res.json({"Spots": userSpots });
+})
+
+//get details of a spot from an id
+router.get('/:spotId', async (req, res, next) => {
+    const spotFromId = await Spot.findOne({
+        where: {
+            id: req.params.spotId
+        },
+        include: [ Image, User, Review ]
+    });
+
+    if (!spotFromId) {
+        res.status(404);
+        res.json({
+            "message": "Spot couldn't be found"
+          })
+    }
+    const owner = await User.findOne({
+        where: {
+            id: spotFromId.ownerId
+        },
+        attributes: {
+            exclude: ['username']
+        }
+    })
+
+    const spotImages = await Image.findAll({
+        where: {
+            imageableId: req.params.spotId
+        },
+        attributes: {
+            exclude: ['imageableId', 'imageableType', 'createdAt', 'updatedAt']
+        }
+    });
+
+    const spotCopy = spotFromId.toJSON();
+
+    spotCopy.numReviews = spotFromId.Reviews.length;
+
+    let starsArr = [];
+
+    for (let review of spotFromId.Reviews) {
+        starRating = review.stars;
+        starsArr.push(starRating);
+    }
+
+    const sumStars = starsArr.reduce(
+        (acc, curr) => acc + curr,);
+
+    spotCopy.avgStarRating = sumStars/spotCopy.numReviews;
+
+    delete spotCopy.Reviews;
+
+    spotCopy.SpotImages = spotImages
+    delete spotCopy.Images
+
+    spotCopy.Owner = owner
+    delete spotCopy.User
+
+    res.json(spotCopy);
+    // res.json(spotImages);
+    // res.json(starsArr);
+
+
 })
 
 module.exports = router;
