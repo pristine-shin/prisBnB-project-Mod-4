1c5999775bfe2410a66c830bbdc4f669ce943f87
Pristine Shin <prisvsyo@gmail.com>
2024-07-25 14:20:15 -0700

    completed basic delete a spot route without authorization

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -15,8 +15,7 @@ router.get('/', async (req, res, next) => {
                 model: Review
             },
             {
-                model: Image,
-                where: { imageableType: 'spot'}
+                model: Image
             }
         ]
     });
@@ -47,14 +46,18 @@ router.get('/', async (req, res, next) => {
 })
 
 //get all spots owned/created by the current user ********************
-router.get('/current', async (req, res, next) => {
-    const userSpots = await Spot.findAll({
-        where: {
-            //not sure about this one
-        }
-    });
+router.get('/current', async (req, res) => {
+    const { user } = req;
 
-    res.json({"Spots": userSpots });
+    if (user) {
+        const userSpots = await Spot.findAll({
+            where: {
+                ownerId: user.id
+            }
+        })
+
+        res.json({ Spots: userSpots });
+    } else res.json({ user: null })
 })
 
 //get details of a spot from an id
@@ -117,8 +120,81 @@ router.get('/:spotId', async (req, res, next) => {
     res.json(spotCopy);
     // res.json(spotImages);
     // res.json(starsArr);
-
-
 })
 
+//create a spot *******************************************
+// const validateNewSpot = [
+//     check('address')
+//     .exists({ checkFalsy: true })
+//     .withMessage('Street address is required'),
+//   check('city')
+//     .exists({ checkFalsy: true })
+//     // .isLength({ min: 4 })
+//     .withMessage('City is required'),
+//   check('state')
+//     .withMessage('State is required'),
+//   check('country')
+//     .exists({ checkFalsy: true })
+//     .withMessage('Country is required'),
+//   check('lat')
+//     .exists({ checkFalsy: true }),
+//     .max({}),
+//     .withMessage('Country is required'),
+//   check('country')
+//     .exists({ checkFalsy: true })
+//     .withMessage('Country is required'),
+//   check('country')
+//     .exists({ checkFalsy: true })
+//     .withMessage('Country is required'),
+//   check('country')
+//     .exists({ checkFalsy: true })
+//     .withMessage('Country is required'),
+//   handleValidationErrors
+// ];
+
+router.post('/', /* validateNewSpot, */
+    async (req, res) => {
+        const { address, city, state, country, lat, lng, name, description, price } = req.body;
+        const spot = await Spot.create({ ownerId: 1, address, city, state, country, lat, lng, name, description, price });
+
+        const validSpot = {
+            ownerId: 1,
+            address: spot.address,
+            city: spot.city,
+            state: spot.state,
+            country: spot.country,
+            lat: spot.lat,
+            lng: spot.lng,
+            name: spot.name,
+            description: spot.description,
+            price: spot.price,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }
+
+        return res.json(spot)
+    }
+)
+
+//delete a spot ***********************************************
+router.delete('/:spotId', async (req, res, next) => {
+    const spotFromId = await Spot.findOne({
+        where: {
+            id: req.params.spotId
+        },
+        // include: [ Image, User, Review ]
+    });
+
+    if (!spotFromId) {
+        res.status(404);
+        res.json({
+            "message": "Spot couldn't be found"
+          })
+    }
+
+    await spotFromId.destroy();
+    res.status(200);
+    res.json({ "message": "Successfully deleted" })
+
+})
 module.exports = router;
