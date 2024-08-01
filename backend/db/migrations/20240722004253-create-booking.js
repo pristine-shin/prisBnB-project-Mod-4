3eee87b60dae811e483e8ec5c358d37cead5a4e7
Pristine Shin <prisvsyo@gmail.com>
2024-08-01 10:25:32 -0700

    refactored spots, passing locally

--- backend/db/migrations/20240722004253-create-booking.js
+++ backend/db/migrations/20240722004253-create-booking.js
@@ -21,7 +21,7 @@ module.exports = {
           model: 'Spots',
           key: 'id'
         },
-        // onDelete: 'CASCADE'
+        onDelete: 'CASCADE'
       },
       userId: {
         type: Sequelize.INTEGER,
@@ -29,7 +29,7 @@ module.exports = {
           model: 'Users',
           key: 'id'
         },
-        // onDelete: 'CASCADE'
+        onDelete: 'CASCADE'
       },
       startDate: {
         type: Sequelize.DATE,
