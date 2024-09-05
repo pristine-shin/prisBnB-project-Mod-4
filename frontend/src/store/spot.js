fac002f4bbd154ec83850619ecc4693cf8f5ecb5
Pristine Shin <prisvsyo@gmail.com>
2024-09-05 08:11:44 -0700

    created spot reducer and added it to store, starting to import into App and dispatch

---
+++ frontend/src/store/spot.js
@@ -0,0 +1,39 @@
+import { csrfFetch } from './csrf';
+
+// constant to avoid debugging typos
+const LOAD_SPOTS = 'spot/load_spots'
+
+//regular action creator
+const loadSpots = (spots) => {
+  return {
+    type: ADD_SESSION,
+    spots
+  };
+};
+
+// thunk action creator
+export const getSpots = (spots) => async (dispatch) => {
+  const response = await csrfFetch('/api/spots');
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(loadSpots(data.Spots));
+    return response;
+  }
+}
+
+// reducer
+const initialState = {};
+
+const spotReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOAD_SPOTS: {
+      const newState = {...state, Spots: action.spots};
+      return newState;
+    }
+    default:
+      return state;
+  }
+};
+
+export default spotReducer;
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import sessionReducer from './session';
+import spotReducer from './spot';
 
 const rootReducer = combineReducers({
   // ADD REDUCERS HERE
   session: sessionReducer,
+  spot: spotReducer
 });
 
 let enhancer;
