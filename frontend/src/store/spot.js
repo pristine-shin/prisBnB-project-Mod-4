868e0894c3be2366c9254f65f0d628d350843c59
Pristine <prisvsyo@gmail.com>
2024-09-06 13:29:53 -0700

    v confused about images on form

--- frontend/src/store/spot.js
+++ frontend/src/store/spot.js
@@ -4,6 +4,7 @@ import { csrfFetch } from './csrf';
 const LOAD_SPOTS = 'spot/load_spots';
 const ADD_SPOT = 'spot/add_spot';
 const LOAD_DETAILS = 'spot/load_details';
+const ADD_IMAGE = 'spot/add_image';
 
 //regular action creator
 const loadSpots = (spots) => {
@@ -17,6 +18,13 @@ const addSpot = (spot) => {
   return {
     type: ADD_SPOT,
     spot
+  }
+};
+
+const addImage = (spotId) => {
+  return {
+    type: ADD_IMAGE,
+    spotId
   }
 }
 
@@ -51,7 +59,21 @@ export const createSpot = (spot) => async (dispatch) => {
   if (res.ok) {
     const data = await res.json();
     dispatch(addSpot(data.spot));
-    return response;
+    return res;
+  }
+}
+
+export const postSpotImage = (image) => async(dispatch) => {
+  const { url, preview } = image;
+  const res = await csrfFetch(`/api/${spotId}/images`, {
+    method: 'POST',
+    body: JSON.stringify({url, preview})
+  });
+
+  if (res.ok) {
+    const data = await res.json();
+    dispatch(addImage(data));
+    return res;
   }
 }
 
@@ -77,6 +99,9 @@ const spotReducer = (state = initialState, action) => {
     case ADD_SPOT: {
       const newState = {...state, ...action.spot.spot};
       return newState;
+    }
+    case ADD_IMAGE: {
+      const newState = {...state, }
     }
     case LOAD_DETAILS: {
       const newState = {...state, ...action.spotId};//dont think this will do it but lets see
