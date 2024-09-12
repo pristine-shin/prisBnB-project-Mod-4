24a5bba619ba941a8dca3009c4789b1d0f1aa1e7
Pristine <prisvsyo@gmail.com>
2024-09-12 11:03:54 -0700

    create new spot button on manage spots page is working

--- frontend/src/components/ManageSpotsPage/ManageSpotsPage.jsx
+++ frontend/src/components/ManageSpotsPage/ManageSpotsPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { getCurrentUserSpots } from "../../store/spot";
 import { useDispatch, useSelector } from "react-redux";
 import { MdStarRate } from "react-icons/md";
@@ -7,11 +7,17 @@ import './ManageSpots.css'
 
 const ManageSpotsPage = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const currUserSpots = useSelector(state => state.spot.Spots)
 
   useEffect(() => {
     dispatch(getCurrentUserSpots());
   }, [dispatch]);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    return navigate('/spots/new');
+  }
 
   if (!currUserSpots) {
     return (
@@ -19,19 +25,23 @@ const ManageSpotsPage = () => {
     )
   }
   return (
-    <div className="all-spots-container">
-      {currUserSpots.map(({ id, previewImage, city, state, avgRating, price }) => (
-        <div key={id} className="spot-card">
-          <NavLink to={`/spots/${id}`} className="spot-link">
-            <img src={previewImage} alt="spot-image" className="spot-image"/>
-            <div className="spot-info">
-              <div>{city}, {state}</div>
-              <div><MdStarRate />{avgRating ? avgRating.toFixed(2) : avgRating}</div>
-            </div>
-            <div className="spot-price">${price} night</div>
-          </NavLink>
-        </div>
-      ))}
+    <div className="manage-spots-page-container">
+      <h1>Manage Your Spots</h1>
+      <button onClick={handleClick}>Create a New Spot</button>
+      <div className="all-spots-container">
+        {currUserSpots.map(({ id, previewImage, city, state, avgRating, price }) => (
+          <div key={id} className="spot-card">
+            <NavLink to={`/spots/${id}`} className="spot-link">
+              <img src={previewImage} alt="spot-image" className="spot-image"/>
+              <div className="spot-info">
+                <div>{city}, {state}</div>
+                <div><MdStarRate />{avgRating ? avgRating.toFixed(2) : avgRating}</div>
+              </div>
+              <div className="spot-price">${price} night</div>
+            </NavLink>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
